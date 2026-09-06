f0a6dc66ca5f7e713ec71209306fc4bfd866c17c
martin <martingrombanovski@yahoo.com>
2026-09-06 18:43:59 +0200

    Initial structure

---
+++ item_service/src/test/java/ds_bidding_system/item_service/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ds_bidding_system.item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
